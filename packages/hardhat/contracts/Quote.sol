18ffe7dc62fb952d2c0eaa24c71f223c7d4def18
Auryn Macmillan <aurynmacmillan@gmail.com>
2021-03-16 22:34:44 -0400

    Add quote contract

---
+++ packages/hardhat/contracts/Quote.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract Quote is Ownable, ERC1155 {
+
+  mapping (uint256 => bool) minted;
+
+  event TokenMinted(string quote, uint256 id);
+
+  constructor() public ERC1155("https://game.example/api/item/{id}.json") {
+  }
+
+  // allows anyone to mint a new quote
+  function mint(string memory quote) public {
+    uint256 id = uint256(keccak256(bytes(quote)));
+    require(!minted[id], "Already minted");
+    _mint(msg.sender, id, 1, "");
+    minted[id] = true;
+    emit TokenMinted(quote, id);
+  }
+}
